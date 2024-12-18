407ddeaaae30a958dd935dbecb15464996eba947
fileuplaoderbot <tparveen2728@gmail.com>
2024-12-18 18:05:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let isResponseGenerating = false;
 
 // API configuration
 const API_KEY = "AIzaSyDYlZHsMfsN-f62bLCyH8m3oyKgta69USs"; // Your API key here
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyDYlZHsMfsN-f62bLCyH8m3oyKgta69USs"}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
 const loadDataFromLocalstorage = () => {
